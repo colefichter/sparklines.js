085520c2a0cb0a8321deb028a5ed9943a9f46764
Cole Fichter <cole.fichter@gmail.com>
2012-08-27 14:17:09 -0600

    Added option to highlight final point, as in Tufte's original sparkline design.

--- sparklines.js
+++ sparklines.js
@@ -24,180 +24,190 @@ if (!Array.prototype.map)
 }
 
 
-var BaseSparkline = function() {
-  this.init = function(id,data,mixins) {
-    this.background_color = 50;
-    this.line_color = "rgba(230,230,230,0.70);";
-    this.percentile_line_color = "#5555FF";
-    this.percentile_fill_color = 75;
-    this.value_line_color = "#7777FF";
-    this.value_line_fill_color = 85;
-    this.canvas = document.getElementById(id);
-    this.data = data;
-    this.scale_from = undefined;
-    this.scale_to = undefined;
-    this.top_padding = 10;
-    this.bottom_padding = 10;
-    this.left_padding = 10;
-    this.right_padding = 10;
-    this.percentile_lines = [];
-    this.fill_between_percentile_lines = false;
-    this.value_lines = [];
-    this.fill_between_value_lines = false;
-    for (var property in mixins) this[property] = mixins[property];
-  };
+var BaseSparkline = function () {
+    this.init = function (id, data, mixins) {
+        this.background_color = 50;
+        this.line_color = "rgba(230,230,230,0.70);";
+        this.percentile_line_color = "#5555FF";
+        this.percentile_fill_color = 75;
+        this.value_line_color = "#7777FF";
+        this.value_line_fill_color = 85;
+        this.canvas = document.getElementById(id);
+        this.data = data;
+        this.scale_from = undefined;
+        this.scale_to = undefined;
+        this.top_padding = 10;
+        this.bottom_padding = 10;
+        this.left_padding = 10;
+        this.right_padding = 10;
+        this.percentile_lines = [];
+        this.fill_between_percentile_lines = false;
+        this.value_lines = [];
+        this.fill_between_value_lines = false;
+        this.last_point_color = undefined;
 
-  /* Extracts height from a piece of data */
-  this.parse_height = function(x) { return x; };
-  this.heights = function() {
-    return this.data.map(this.parse_height);
-  };
-
-  this.max = function() {
-    var vals = this.heights();
-    var max = vals[0];
-    var l = vals.length;
-    for (var i=1; i<l; i++) max = Math.max(max, vals[i]);
-    return max;
-  };
-  this.min = function() {
-    var vals = this.heights();
-    var min = vals[0];
-    var l = vals.length;
-    for (var i=1; i<l; i++) min= Math.min(min, vals[i]);
-    return min;
-  };
-  this.height = function() {
-    return this.canvas.height - this.top_padding - this.bottom_padding;
-  };
-  this.width = function() {
-    return this.canvas.width - this.left_padding - this.right_padding;
-  };
-  this.scale_values = function(values, max) {
-    if (!max) max = this.max();
-    var p = this.top_padding;
-    var h = this.height();
-    var top = (this.scale_to != undefined) ? this.scale_to : max;
-    var bottom = (this.scale_from != undefined) ? this.scale_from : this.min();
-    var range = Math.abs(top - bottom);
-    var scale = function(x) {
-      var percentage = ((x-bottom)*1.0) / range;
-      return h - (h * percentage) + p;
+        for (var property in mixins) this[property] = mixins[property];
     };
-    return values.map(scale, this);
-  };
 
-
-  this.calc_value_lines = function() {
-    var scaled = this.scale_values(this.value_lines);
-    scaled.sort(function(a,b) { return a-b; });
-    return scaled;
-  };
-
-  this.calc_percentiles = function() {
-    var sorted = this.heights();
-    sorted.sort(function(a,b) { return a-b; });
-
-    // Find data points at percentages.
-    var points = [];
-    var n = sorted.length;
-    var l = this.percentile_lines.length;
-    for (var i=0;i<l;i++) {
-      var percentage = this.percentile_lines[i];
-      var position = Math.round(percentage*(n+1));
-      points.push(sorted[position]);
-    }
-
-    // Scale data points to size.
-    var max = sorted[n-1];
-    var raws = this.scale_values(points, max);
-    raws.sort(function(a,b) { return a-b; });
-    return raws;
-  };
-
-  this.scale_height = function() {
-    return this.scale_values(this.heights());
-  };
-
-  this.segment_width = function() {
-    var w = this.width();
-    var l = this.data.length;
-    return (w * 1.0) / (l-1);
-  };
-  this.scale_width = function() {
-    var widths = [];
-    var l = this.data.length;
-    var segment_width = this.segment_width();
-    for (var i=0; i<l; i++) {
-      widths.push((i*segment_width)+this.left_padding);
-    }
-    return widths;
-  };
-  this.scale_data = function() {
-    var heights = this.scale_height();
-    var widths = this.scale_width();
-    var l = heights.length;
-    var data = [];
-    for (var i=0;i<l;i++)
-      data.push({'y':heights[i], 'x':widths[i]});
-    return data;
-  };
-
-  this.draw = function() {
-    var sl = this;
-    with(Processing(sl.canvas)) {
-      setup = function() {};
-      draw = function() {
-	background(sl.background_color);
-	scaled = sl.scale_data();
-	var l = scaled.length;
-
-	var percentiles = sl.calc_percentiles();
-	// Draw fill between percentile lines, if applicable.
-	if (sl.fill_between_percentile_lines && percentiles.length > 1) {
-	  noStroke();
-	  fill(sl.percentile_fill_color);
-	  var height = percentiles[percentiles.length-1] - percentiles[0];
-	  var width = scaled[l-1].x - scaled[0].x;
-	  rect(scaled[0].x, percentiles[0], width, height);
-	}
-
-	var value_lines = sl.calc_value_lines();
-	// Draw fill between value lines, if applicable.
-	if (sl.fill_between_value_lines && value_lines.length > 1) {
-	  noStroke();
-	  fill(sl.value_line_fill_color);
-	  var height = value_lines[value_lines.length-1] - value_lines[0];
-	  var width = scaled[l-1].x - scaled[0].x;
-	  rect(scaled[0].x, value_lines[0], width, height);
-	}
-
-	// Draw value lines.
-	stroke(sl.value_line_color);
-	for (var h=0;h<value_lines.length;h++) {
-	  var y = value_lines[h];
-	  line(scaled[0].x,y,scaled[l-1].x,y);
-	}
-
-	// Draw percentile lines.
-	stroke(sl.percentile_line_color);
-	for (var j=0;j<percentiles.length;j++) {
-	  var y = percentiles[j];
-	  line(scaled[0].x,y,scaled[l-1].x,y);
-	}
-
-	// Draw lines between data points.
-	stroke(sl.line_color);
-	for (var i=1; i<l;i++) {
-	  var curr = scaled[i];
-	  var previous = scaled[i-1];
-	  line(previous.x,previous.y,curr.x,curr.y);
-	}
-	this.exit();
-      };
-      init();
+    /* Extracts height from a piece of data */
+    this.parse_height = function (x) { return x; };
+    this.heights = function () {
+        return this.data.map(this.parse_height);
     };
-  };
+
+    this.max = function () {
+        var vals = this.heights();
+        var max = vals[0];
+        var l = vals.length;
+        for (var i = 1; i < l; i++) max = Math.max(max, vals[i]);
+        return max;
+    };
+    this.min = function () {
+        var vals = this.heights();
+        var min = vals[0];
+        var l = vals.length;
+        for (var i = 1; i < l; i++) min = Math.min(min, vals[i]);
+        return min;
+    };
+    this.height = function () {
+        return this.canvas.height - this.top_padding - this.bottom_padding;
+    };
+    this.width = function () {
+        return this.canvas.width - this.left_padding - this.right_padding;
+    };
+    this.scale_values = function (values, max) {
+        if (!max) max = this.max();
+        var p = this.top_padding;
+        var h = this.height();
+        var top = (this.scale_to != undefined) ? this.scale_to : max;
+        var bottom = (this.scale_from != undefined) ? this.scale_from : this.min();
+        var range = Math.abs(top - bottom);
+        var scale = function (x) {
+            var percentage = ((x - bottom) * 1.0) / range;
+            return h - (h * percentage) + p;
+        };
+        return values.map(scale, this);
+    };
+
+
+    this.calc_value_lines = function () {
+        var scaled = this.scale_values(this.value_lines);
+        scaled.sort(function (a, b) { return a - b; });
+        return scaled;
+    };
+
+    this.calc_percentiles = function () {
+        var sorted = this.heights();
+        sorted.sort(function (a, b) { return a - b; });
+
+        // Find data points at percentages.
+        var points = [];
+        var n = sorted.length;
+        var l = this.percentile_lines.length;
+        for (var i = 0; i < l; i++) {
+            var percentage = this.percentile_lines[i];
+            var position = Math.round(percentage * (n + 1));
+            points.push(sorted[position]);
+        }
+
+        // Scale data points to size.
+        var max = sorted[n - 1];
+        var raws = this.scale_values(points, max);
+        raws.sort(function (a, b) { return a - b; });
+        return raws;
+    };
+
+    this.scale_height = function () {
+        return this.scale_values(this.heights());
+    };
+
+    this.segment_width = function () {
+        var w = this.width();
+        var l = this.data.length;
+        return (w * 1.0) / (l - 1);
+    };
+    this.scale_width = function () {
+        var widths = [];
+        var l = this.data.length;
+        var segment_width = this.segment_width();
+        for (var i = 0; i < l; i++) {
+            widths.push((i * segment_width) + this.left_padding);
+        }
+        return widths;
+    };
+    this.scale_data = function () {
+        var heights = this.scale_height();
+        var widths = this.scale_width();
+        var l = heights.length;
+        var data = [];
+        for (var i = 0; i < l; i++)
+            data.push({ 'y': heights[i], 'x': widths[i] });
+        return data;
+    };
+
+    this.draw = function () {
+        var sl = this;
+        with (Processing(sl.canvas)) {
+            setup = function () { };
+            draw = function () {
+                background(sl.background_color);
+                scaled = sl.scale_data();
+                var l = scaled.length;
+
+                var percentiles = sl.calc_percentiles();
+                // Draw fill between percentile lines, if applicable.
+                if (sl.fill_between_percentile_lines && percentiles.length > 1) {
+                    noStroke();
+                    fill(sl.percentile_fill_color);
+                    var height = percentiles[percentiles.length - 1] - percentiles[0];
+                    var width = scaled[l - 1].x - scaled[0].x;
+                    rect(scaled[0].x, percentiles[0], width, height);
+                }
+
+                var value_lines = sl.calc_value_lines();
+                // Draw fill between value lines, if applicable.
+                if (sl.fill_between_value_lines && value_lines.length > 1) {
+                    noStroke();
+                    fill(sl.value_line_fill_color);
+                    var height = value_lines[value_lines.length - 1] - value_lines[0];
+                    var width = scaled[l - 1].x - scaled[0].x;
+                    rect(scaled[0].x, value_lines[0], width, height);
+                }
+
+                // Draw value lines.
+                stroke(sl.value_line_color);
+                for (var h = 0; h < value_lines.length; h++) {
+                    var y = value_lines[h];
+                    line(scaled[0].x, y, scaled[l - 1].x, y);
+                }
+
+                // Draw percentile lines.
+                stroke(sl.percentile_line_color);
+                for (var j = 0; j < percentiles.length; j++) {
+                    var y = percentiles[j];
+                    line(scaled[0].x, y, scaled[l - 1].x, y);
+                }
+
+                // Draw lines between data points.
+                stroke(sl.line_color);
+                for (var i = 1; i < l; i++) {
+                    var curr = scaled[i];
+                    var previous = scaled[i - 1];
+                    line(previous.x, previous.y, curr.x, curr.y);
+                }
+
+                if (sl.last_point_color && scaled.length > 0) {
+                    fill(sl.last_point_color);
+                    stroke(sl.last_point_color);
+                    var point = scaled[scaled.length - 1];
+                    ellipse(point.x - 1, point.y, 3, 3);
+                }
+
+                this.exit();
+            };
+            init();
+        };
+    };
 };
 
 var Sparkline = function(id,data,mixins) {
